7d991d6a572a8d1a77106a4c61becacfe60ce697
FunckerAdv <68787846+FunckerAdv@users.noreply.github.com>
2026-07-07 04:40:52 +0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -4,18 +4,6 @@ AddEventHandler('playerSpawned', function()
     TriggerServerEvent('vorp:checkAndFixVW')
 end)
 
--- Verificarea pe client a fost Eliminata!
---[[
-Citizen.CreateThread(function()
-    Wait(math.random(5000, 15000)) 
-    while true do
-        Wait(math.random(20000, 40000)) 
-        TriggerServerEvent('vorp:checkAndFixVW') 
-    end
-end)
-]]
-
-
 RegisterNetEvent('vorp:clientDebug')
 AddEventHandler('vorp:clientDebug', function(msg)
     print(msg)
